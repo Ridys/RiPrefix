400e0831e333a9c5aba9029712a96b8f49e5505b
Ridys <dimasir00@yandex.ru>
2017-11-28 21:03:12 +0700

    Added commands

---
+++ RiPrefix/src/me/ridys/RiPrefix/Command.java
@@ -0,0 +1,5 @@
+package me.ridys.RiPrefix;
+
+public class Command {
+
+}
--- RiPrefix/src/me/ridys/RiPrefix/RiPrefix.java
+++ RiPrefix/src/me/ridys/RiPrefix/RiPrefix.java
@@ -1,5 +1,26 @@
 package me.ridys.RiPrefix;
 
-public class RiPrefix {
+import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RiPrefix extends JavaPlugin {
+	Logger log = getLogger();
+
+	public void onEnable(){
+		log.info("RiPrefix ready!");
+	}
+
+	public void onDisable(){
+		log.info("RiPrefix disabled!");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		if(cmd.getName().equalsIgnoreCase("rips")){
+			return true;
+		}
+		return false; 
+	}
 }
