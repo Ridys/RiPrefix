af31c05461136578a622fb3e14dba6c981b33562
Ridys <dimasir00@yandex.ru>
2017-11-29 00:31:00 +0700

    Some plugin commands (dev.)

--- RiPrefix/src/me/ridys/RiPrefix/MainCmd.java
+++ RiPrefix/src/me/ridys/RiPrefix/MainCmd.java
@@ -7,7 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class MainCmd implements CommandExecutor {
-    private RiPrefix plugin;
+	@SuppressWarnings("unused")
+	private RiPrefix plugin;
     public MainCmd(RiPrefix plugin) {
         this.plugin = plugin;
     }
@@ -17,15 +18,46 @@ public class MainCmd implements CommandExecutor {
         if (sender instanceof Player) {
             player = (Player) sender;
         }
-        if (player == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "This command can only be run by a player");    
-        } else {
-            player.sendMessage(ChatColor.GREEN + "[RiPrefix]" + ChatColor.BLUE + " Commands Help");
-            player.sendMessage(ChatColor.GOLD + "/rips" + ChatColor.RED + " - This help page");
-            player.sendMessage(ChatColor.GOLD + "/rips [Prefix]" + ChatColor.RED + " - Set prefix for you");
-            player.sendMessage(ChatColor.GOLD + "/rips set [Player] [Prefix]" + ChatColor.RED + " - Set player prefix");
-            player.sendMessage(ChatColor.GOLD + "/rips reset [Player]" + ChatColor.RED + " - Reset player prefix");
+        if (args.length == 0) {
+	        if (player == null) {
+	            sender.sendMessage(ChatColor.DARK_RED + "This command can only be run by a player");    
+	        } else {
+	            player.sendMessage(ChatColor.GREEN + "[RiPrefix]" + ChatColor.BLUE + " Commands Help");
+	            player.sendMessage(ChatColor.GOLD + "/rips" + ChatColor.RED + " - This help page");
+	            player.sendMessage(ChatColor.GOLD + "/rips me [Prefix]" + ChatColor.RED + " - Set prefix for you");
+	            player.sendMessage(ChatColor.GOLD + "/rips set [Player] [Prefix]" + ChatColor.RED + " - Set player prefix");
+	            player.sendMessage(ChatColor.GOLD + "/rips reset [Player]" + ChatColor.RED + " - Reset player prefix");
+	        }
+	        return true;
         }
-        return true;
+        if (args[0].equalsIgnoreCase("me") && args.length > 1 && args[1] != null && player.hasPermission("riprefix.me")) {
+	        player.sendMessage("Me command: " + args[1]);
+	        return true;
+        }
+        if (args[0].equalsIgnoreCase("set") && args.length > 2 && args[1] != null && args[2] != null) {
+        	if (player == null) {
+        		sender.sendMessage("Set command: " + args[1] + args[2]);
+        	} else {
+        		if (player.hasPermission("riprefix.set")) {
+        			sender.sendMessage("Set command: " + args[1] + args[2]);
+        		} else {
+        			sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+        		}
+        	}
+	        return true;
+        }
+        if (args[0].equalsIgnoreCase("reset") && args.length > 1 && args[1] != null) {
+        	if (player == null) {
+        		sender.sendMessage("Reset command: " + args[1]);
+        	} else {
+        		if (player.hasPermission("riprefix.reset")) {
+        			sender.sendMessage("Reset command: " + args[1]);
+        		} else {
+        			sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+        		}
+        	}
+	        return true;
+        }
+		return false;
     }
 }
