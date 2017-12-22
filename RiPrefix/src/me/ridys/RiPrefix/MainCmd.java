01d2f89738de752bf9b3b806601efc72771e08d1
Ridys <dimasir00@yandex.ru>
2017-12-22 18:28:47 +0700

    Configure functions with Vault, fix #1

--- RiPrefix/src/me/ridys/RiPrefix/MainCmd.java
+++ RiPrefix/src/me/ridys/RiPrefix/MainCmd.java
@@ -21,6 +21,7 @@ public class MainCmd implements CommandExecutor {
         if (args.length == 0) {  
 	        sender.sendMessage(ChatColor.GREEN + "[RiPrefix] " + ChatColor.BLUE + plugin.getConfig().getString("lang.help_title"));
 	        sender.sendMessage(ChatColor.GOLD + "/rips " + ChatColor.RED + plugin.getConfig().getString("lang.help_help"));
+	        sender.sendMessage(ChatColor.GOLD + "/rips clear " + ChatColor.RED + plugin.getConfig().getString("lang.help_clear"));
 	        sender.sendMessage(ChatColor.GOLD + "/rips me [Prefix] " + ChatColor.RED + plugin.getConfig().getString("lang.help_me"));
 	        sender.sendMessage(ChatColor.GOLD + "/rips set [Player] [Prefix] " + ChatColor.RED + plugin.getConfig().getString("lang.help_set"));
 	        sender.sendMessage(ChatColor.GOLD + "/rips reset [Player] " + ChatColor.RED + plugin.getConfig().getString("lang.help_reset"));
@@ -28,19 +29,25 @@ public class MainCmd implements CommandExecutor {
 	        return true;
         }
         if (args[0].equalsIgnoreCase("me") && args.length > 1 && args[1] != null && player.hasPermission("riprefix.me")) {
-        	if(ChatHandler.meCMD(player, args[1])) {
-        		sender.sendMessage(plugin.getConfig().getString("lang.changed"));
-        	} else {
-        		sender.sendMessage(plugin.getConfig().getString("lang.deleted"));
-        	}
+        	ChatHandler.meCMD(player, args[1]);
+        	sender.sendMessage(plugin.getConfig().getString("lang.changed"));
+	        return true;
+        }
+        if (args[0].equalsIgnoreCase("clear") && player.hasPermission("riprefix.clear")) {
+        	ChatHandler.clearCMD(player);
+        	sender.sendMessage(plugin.getConfig().getString("lang.deleted"));
 	        return true;
         }
         if (args[0].equalsIgnoreCase("set") && args.length > 2 && args[1] != null && args[2] != null) {
         	if (player == null) {
-            	// set console command here
+        		if (ChatHandler.setCMD(args[1], args[2])) {
+        			sender.sendMessage(plugin.getConfig().getString("lang.opchange"));
+        		} else { sender.sendMessage(plugin.getConfig().getString("lang.corrupted")); }
             } else {
             	if (player.hasPermission("riprefix.set")) {
-        			// set player command here
+            		if (ChatHandler.setCMD(args[1], args[2])) {
+            			sender.sendMessage(plugin.getConfig().getString("lang.opchange"));
+            		} else { sender.sendMessage(plugin.getConfig().getString("lang.corrupted")); }
         		} else {
         			sender.sendMessage(ChatColor.RED + p);
         		}
@@ -49,10 +56,14 @@ public class MainCmd implements CommandExecutor {
         }
         if (args[0].equalsIgnoreCase("reset") && args.length > 1 && args[1] != null) {
         	if (player == null) {
-            	// console reset command
+        		if (ChatHandler.resetCMD(args[1])) {
+        			sender.sendMessage(plugin.getConfig().getString("lang.reset"));
+        		} else { sender.sendMessage(plugin.getConfig().getString("lang.corrupted")); }
         	} else {
         		if (player.hasPermission("riprefix.reset")) {
-                	// player reset command
+            		if (ChatHandler.resetCMD(args[1])) {
+            			sender.sendMessage(plugin.getConfig().getString("lang.reset"));
+            		} else { sender.sendMessage(plugin.getConfig().getString("lang.corrupted")); }
         		} else {
         			sender.sendMessage(ChatColor.RED + p);
         		}
